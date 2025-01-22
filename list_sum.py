b3ab5d6e25388897327069d9bf553256d6e568e3
1MIIRO <94036345+1MIIRO@users.noreply.github.com>
2025-01-22 14:51:51 +0300

    Add files via upload

--- list_sum.py
+++ list_sum.py
@@ -1,6 +1,12 @@
 demolist =[1,89, 23.4, 100, "23%", 92, "88A"]
-def get_total_value(list):
-    return sum(num for num in list if isinstance(num, (int, float)))
 
-answer_2=get_total_value(demolist)
+
+def get_total_value(list):
+    i = 0
+    for num in list:
+        if isinstance(num, (int, float)):
+            i += num
+    return i
+
+answer_2 = get_total_value(demolist)
 print(answer_2)
